ec951020924ca53bb9de1a7287df9282f44e1ace
astrofra <astrofra@gmail.com>
2016-07-16 22:17:43 +0200

    - Fixed the missing flipped pipes, using a copy of each bitmap, flipped directly in the file.  - Removed ~25 lines of useless code.

--- flappy.py
+++ flappy.py
@@ -6,12 +6,11 @@ import gs
 import gs.plus.render as render
 import gs.plus.clock as clock
 
-FPS = 30
-SCREENWIDTH  = 288
+SCREENWIDTH = 288
 SCREENHEIGHT = 512
 # amount by which base can maximum shift to left
-PIPEGAPSIZE  = 100 # gap between upper and lower part of pipe
-BASEY        = SCREENHEIGHT * 0.79
+PIPEGAPSIZE = 100 # gap between upper and lower part of pipe
+BASEY = SCREENHEIGHT * 0.79
 # image, sound and hitmask  dicts
 IMAGES, SOUNDS, HITMASKS = {}, {}, {}
 
@@ -48,6 +47,8 @@ BACKGROUNDS_LIST = (
 PIPES_LIST = (
 	'@assets/sprites/pipe-green.png',
 	'@assets/sprites/pipe-red.png',
+	'@assets/sprites/pipe-green-yflip.png',
+	'@assets/sprites/pipe-red-yflip.png',	
 )
 
 
@@ -61,7 +62,7 @@ def sprite2dtex(tex, x: float, y: float, size: float = 1.0, color=gs.Color.White
 
 
 def main():
-	global al, keyboard #, SCREEN, FPSCLOCK
+	global al, keyboard
 
 	# load plugins
 	gs.LoadPlugins(gs.get_default_plugins_path())
@@ -76,80 +77,58 @@ def main():
 	# open keyboard device
 	keyboard = gs.GetInputSystem().GetDevice("keyboard")
 
-	# FPSCLOCK = pygame.time.Clock()
-	# SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-	# pygame.display.set_caption('Flappy Bird')
-
 	# numbers sprites for score display
-	IMAGES['numbers'] = (
-		render.get_renderer().LoadTexture('@assets/sprites/0.png'), #.convert_alpha(),
-		render.get_renderer().LoadTexture('@assets/sprites/1.png'), #.convert_alpha(),
-		render.get_renderer().LoadTexture('@assets/sprites/2.png'), #.convert_alpha(),
-		render.get_renderer().LoadTexture('@assets/sprites/3.png'), #.convert_alpha(),
-		render.get_renderer().LoadTexture('@assets/sprites/4.png'), #.convert_alpha(),
-		render.get_renderer().LoadTexture('@assets/sprites/5.png'), #.convert_alpha(),
-		render.get_renderer().LoadTexture('@assets/sprites/6.png'), #.convert_alpha(),
-		render.get_renderer().LoadTexture('@assets/sprites/7.png'), #.convert_alpha(),
-		render.get_renderer().LoadTexture('@assets/sprites/8.png'), #.convert_alpha(),
-		render.get_renderer().LoadTexture('@assets/sprites/9.png') #.convert_alpha()
-	)
+	IMAGES['numbers'] = []
+	for i in range(9):
+		IMAGES['numbers'].append(render.get_renderer().LoadTexture('@assets/sprites/{0}.png'.format(i)))
 
-	# game over sprite
-	IMAGES['gameover'] = render.get_renderer().LoadTexture('@assets/sprites/gameover.png') #.convert_alpha()
-	# message sprite for welcome screen
-	IMAGES['message'] = render.get_renderer().LoadTexture('@assets/sprites/message.png') #.convert_alpha()
-	# base (ground) sprite
-	IMAGES['base'] = render.get_renderer().LoadTexture('@assets/sprites/base.png') #.convert_alpha()
+	# game over, message and ground sprites
+	for key in ['gameover', 'message', 'base']:
+		IMAGES[key] = render.get_renderer().LoadTexture('@assets/sprites/' + key + '.png')
 
 	# sounds
-	sound_ext = '.wav'
-
-	SOUNDS['die'] = al.LoadSound('@assets/audio/die' + sound_ext)
-	SOUNDS['hit'] = al.LoadSound('@assets/audio/hit' + sound_ext)
-	SOUNDS['point'] = al.LoadSound('@assets/audio/point' + sound_ext)
-	SOUNDS['swoosh'] = al.LoadSound('@assets/audio/swoosh' + sound_ext)
-	SOUNDS['wing'] = al.LoadSound('@assets/audio/wing' + sound_ext)
+	for key in ['die', 'hit', 'point', 'swoosh', 'wing']:
+		SOUNDS[key] = al.LoadSound('@assets/audio/' + key + '.wav')
 
 	while True:
 		# select random background sprites
 		rand_bg = random.randint(0, len(BACKGROUNDS_LIST) - 1)
-		IMAGES['background'] = render.get_renderer().LoadTexture(BACKGROUNDS_LIST[rand_bg]) #.convert()
+		IMAGES['background'] = render.get_renderer().LoadTexture(BACKGROUNDS_LIST[rand_bg]) 
 
 		# select random player sprites
 		rand_player = random.randint(0, len(PLAYERS_LIST) - 1)
 		IMAGES['player'] = (
-			render.get_renderer().LoadTexture(PLAYERS_LIST[rand_player][0]), #.convert_alpha(),
-			render.get_renderer().LoadTexture(PLAYERS_LIST[rand_player][1]), #.convert_alpha(),
-			render.get_renderer().LoadTexture(PLAYERS_LIST[rand_player][2]) #.convert_alpha(),
+			render.get_renderer().LoadTexture(PLAYERS_LIST[rand_player][0]),
+			render.get_renderer().LoadTexture(PLAYERS_LIST[rand_player][1]),
+			render.get_renderer().LoadTexture(PLAYERS_LIST[rand_player][2])
 		)
 
 		# select random pipe sprites
-		pipe_index = random.randint(0, len(PIPES_LIST) - 1)
-		pipe_image = render.get_renderer().LoadTexture(PIPES_LIST[pipe_index])
+		pipe_index = random.randint(0, 1)
 		IMAGES['pipe'] = (
-			pipe_image, # gs.Picture.BlitTransform(pipe_image, pipe_image.GetRect(), gs.RotationMatrixXAxis(math.pi), gs.Filter.Bilinear),
-			pipe_image
+			render.get_renderer().LoadTexture(PIPES_LIST[pipe_index + 2]),
+			render.get_renderer().LoadTexture(PIPES_LIST[pipe_index])
 		)
 
 		# hismask for pipes
 		HITMASKS['pipe'] = (
-			getHitmask(IMAGES['pipe'][0]),
-			getHitmask(IMAGES['pipe'][1]),
+			get_bitmap_hitmask(IMAGES['pipe'][0]),
+			get_bitmap_hitmask(IMAGES['pipe'][1])
 		)
 
 		# hitmask for player
 		HITMASKS['player'] = (
-			getHitmask(IMAGES['player'][0]),
-			getHitmask(IMAGES['player'][1]),
-			getHitmask(IMAGES['player'][2]),
+			get_bitmap_hitmask(IMAGES['player'][0]),
+			get_bitmap_hitmask(IMAGES['player'][1]),
+			get_bitmap_hitmask(IMAGES['player'][2]),
 		)
 
-		movement_info = showWelcomeAnimation()
-		crash_info = mainGame(movement_info)
-		showGameOverScreen(crash_info)
+		movement_info = show_welcome_animation()
+		crash_info = main_game_loop(movement_info)
+		show_game_over_screen(crash_info)
 
 
-def showWelcomeAnimation():
+def show_welcome_animation():
 	"""Shows welcome screen animation of flappy bird"""
 	# index of player to blit on screen
 	player_index = 0
@@ -168,10 +147,9 @@ def showWelcomeAnimation():
 	base_shift = IMAGES['base'].GetWidth() - IMAGES['background'].GetWidth()
 
 	# player shm for up-down motion on welcome screen
-	player_shm_vals = {'val': 0, 'dir': 1}
+	player_frame = {'val': 0, 'dir': 1}
 
 	while True:
-		clock.update()
 		render.clear()
 
 		render.set_blend_mode2d(render.BlendAlpha)
@@ -181,10 +159,10 @@ def showWelcomeAnimation():
 			# pygame.quit()
 			sys.exit()
 		if keyboard.WasPressed(gs.InputDevice.KeySpace):
-			# make first flap sound and return values for mainGame
+			# make first flap sound and return values for main_game_loop
 			al.Start(SOUNDS['wing'])
 			return {
-				'playery': playery + player_shm_vals['val'],
+				'playery': playery + player_frame['val'],
 				'basex': basex,
 				'player_index_gen': player_index_gen,
 			}
@@ -194,21 +172,22 @@ def showWelcomeAnimation():
 			player_index = next(player_index_gen)
 		loop_iter = (loop_iter + 1) % 30
 		basex = -((-basex + 4) % base_shift)
-		playerShm(player_shm_vals)
+		player_oscillate(player_frame)
 
 		# draw sprites
 		sprite2dtex(IMAGES['background'], 0, 0)
 		sprite2dtex(IMAGES['player'][player_index],
-					playerx, playery + player_shm_vals['val'])
+					playerx, playery + player_frame['val'])
 		sprite2dtex(IMAGES['message'], messagex, messagey)
 		sprite2dtex(IMAGES['base'], basex, BASEY)
 
 		render.set_blend_mode2d(render.BlendOpaque)
 
 		render.flip()
+		clock.update()
 
 
-def mainGame(movement_info):
+def main_game_loop(movement_info):
 	score = player_index = loop_iter = 0
 	player_index_gen = movement_info['player_index_gen']
 	playerx, playery = int(SCREENWIDTH * 0.2), movement_info['playery']
@@ -217,8 +196,8 @@ def mainGame(movement_info):
 	base_shift = IMAGES['base'].GetWidth() - IMAGES['background'].GetWidth()
 
 	# get 2 new pipes to add to upper_pipes lower_pipes list
-	new_pipe_1 = getRandomPipe()
-	new_pipe_2 = getRandomPipe()
+	new_pipe_1 = get_random_pipe()
+	new_pipe_2 = get_random_pipe()
 
 	# list of upper pipes
 	upper_pipes = [
@@ -237,7 +216,7 @@ def mainGame(movement_info):
 	# player velocity, max velocity, downward accleration, accleration on flap
 	player_vel_y    =  -9   # player's velocity along Y, default same as player_flapped
 	player_max_vel_y =  10   # max vel along Y, max descend speed
-	player_min_vel_y =  -8   # min vel along Y, max ascend speed
+	# player_min_vel_y =  -8   # min vel along Y, max ascend speed
 	player_acc_y    =   1   # players downward accleration
 	player_flap_acc =  -9   # players speed on flapping
 	player_flapped = False # True when player flaps
@@ -253,7 +232,7 @@ def mainGame(movement_info):
 				al.Start(SOUNDS['wing'])
 
 		# check for crash here
-		crash_test = checkCrash({'x': playerx, 'y': playery, 'index': player_index},
+		crash_test = check_collision({'x': playerx, 'y': playery, 'index': player_index},
 							   upper_pipes, lower_pipes)
 		if crash_test[0]:
 			return {
@@ -295,7 +274,7 @@ def mainGame(movement_info):
 
 		# add new pipe when first pipe is about to touch left of screen
 		if 0 < upper_pipes[0]['x'] < 5:
-			new_pipe = getRandomPipe()
+			new_pipe = get_random_pipe()
 			upper_pipes.append(new_pipe[0])
 			lower_pipes.append(new_pipe[1])
 
@@ -313,15 +292,15 @@ def mainGame(movement_info):
 
 		sprite2dtex(IMAGES['base'], basex, BASEY)
 		# print score so player overlaps the score
-		showScore(score)
+		show_score(score)
 		sprite2dtex(IMAGES['player'][player_index], playerx, playery)
 
 		render.flip()
 		clock.update()
 
 
-def showGameOverScreen(crash_info):
-	"""crashes the player down ans shows gameover image"""
+def show_game_over_screen(crash_info):
+	"""crashes the player down ans shows game over image"""
 	score = crash_info['score']
 	playerx = SCREENWIDTH * 0.2
 	playery = crash_info['y']
@@ -361,54 +340,51 @@ def showGameOverScreen(crash_info):
 			sprite2dtex(IMAGES['pipe'][1], l_pipe['x'], l_pipe['y'])
 
 		sprite2dtex(IMAGES['base'], basex, BASEY)
-		showScore(score)
+		show_score(score)
 		sprite2dtex(IMAGES['player'][1], playerx,playery)
 
 		render.flip()
 		clock.update()
 
 
-def playerShm(playerShm):
+def player_oscillate(playerShm):
 	"""oscillates the value of playerShm['val'] between 8 and -8"""
 	if abs(playerShm['val']) == 8:
 		playerShm['dir'] *= -1
 
-	if playerShm['dir'] == 1:
-		playerShm['val'] += 1
-	else:
-		playerShm['val'] -= 1
+	playerShm['val'] += playerShm['dir']
 
 
-def getRandomPipe():
+def get_random_pipe():
 	"""returns a randomly generated pipe"""
 	# y of gap between upper and lower pipe
 	gap_y = random.randrange(0, int(BASEY * 0.6 - PIPEGAPSIZE))
 	gap_y += int(BASEY * 0.2)
 	pipe_height = IMAGES['pipe'][0].GetHeight()
-	pipeX = SCREENWIDTH + 10
+	pipe_x = SCREENWIDTH + 10
 
 	return [
-		{'x': pipeX, 'y': gap_y - pipe_height},  # upper pipe
-		{'x': pipeX, 'y': gap_y + PIPEGAPSIZE}, # lower pipe
+		{'x': pipe_x, 'y': gap_y - pipe_height},  # upper pipe
+		{'x': pipe_x, 'y': gap_y + PIPEGAPSIZE}   # lower pipe
 	]
 
 
-def showScore(score):
+def show_score(score):
 	"""displays score in center of screen"""
-	scoreDigits = [int(x) for x in list(str(score))]
-	totalWidth = 0 # total width of all numbers to be printed
+	score_digits = [int(x) for x in list(str(score))]
+	total_width = 0 # total width of all numbers to be printed
 
-	for digit in scoreDigits:
-		totalWidth += IMAGES['numbers'][digit].GetWidth()
+	for digit in score_digits:
+		total_width += IMAGES['numbers'][digit].GetWidth()
 
-	Xoffset = (SCREENWIDTH - totalWidth) / 2
+	x_offset = (SCREENWIDTH - total_width) / 2
 
-	for digit in scoreDigits:
-		sprite2dtex(IMAGES['numbers'][digit], Xoffset, SCREENHEIGHT * 0.1)
-		Xoffset += IMAGES['numbers'][digit].GetWidth()
+	for digit in score_digits:
+		sprite2dtex(IMAGES['numbers'][digit], x_offset, SCREENHEIGHT * 0.1)
+		x_offset += IMAGES['numbers'][digit].GetWidth()
 
 
-def checkCrash(player, upper_pipes, lower_pipes):
+def check_collision(player, upper_pipes, lower_pipes):
 	"""returns True if player collders with base or pipes."""
 	pi = player['index']
 	player['w'] = IMAGES['player'][0].GetWidth()
@@ -417,9 +393,9 @@ def checkCrash(player, upper_pipes, lower_pipes):
 	# if player crashes into ground
 	if player['y'] + player['h'] >= BASEY - 1:
 		return [True, True]
-	else:
+	else:   
 
-		playerRect = gs.iRect.FromWidthHeight(player['x'], player['y'],
+		player_rect = gs.iRect.FromWidthHeight(player['x'], player['y'],
 					  player['w'], player['h'])
 		pipeW = IMAGES['pipe'][0].GetWidth()
 		pipeH = IMAGES['pipe'][0].GetHeight()
@@ -435,8 +411,8 @@ def checkCrash(player, upper_pipes, lower_pipes):
 			lHitmask = HITMASKS['pipe'][1]
 
 			# if bird collided with upipe or lpipe
-			uCollide = pixelCollision(playerRect, uPipeRect, pHitMask, uHitmask)
-			lCollide = pixelCollision(playerRect, lPipeRect, pHitMask, lHitmask)
+			uCollide = check_pixel_collision(player_rect, uPipeRect, pHitMask, uHitmask)
+			lCollide = check_pixel_collision(player_rect, lPipeRect, pHitMask, lHitmask)
 
 			if uCollide or lCollide:
 				return [True, False]
@@ -444,7 +420,7 @@ def checkCrash(player, upper_pipes, lower_pipes):
 	return [False, False]
 
 
-def pixelCollision(rect1, rect2, hitmask1, hitmask2):
+def check_pixel_collision(rect1, rect2, hitmask1, hitmask2):
 	"""Checks if two objects collide and not just their rects"""
 	rect = rect1.Intersection(rect2)
 
@@ -459,14 +435,14 @@ def pixelCollision(rect1, rect2, hitmask1, hitmask2):
 			return False
 		return mask[x][y]
 
-	for x in range(rect.GetWidth()):
-		for y in range(rect.GetHeight()):
+	for x in range(0, rect.GetWidth(), 2):
+		for y in range(0, rect.GetHeight(), 2):
 			if get_hitmask_value(hitmask1, x1+x, y1+y) and get_hitmask_value(hitmask2, x2+x, y2+y):
 				return True
 	return False
 
 
-def getHitmask(tex):
+def get_bitmap_hitmask(tex):
 	"""returns a hitmask using an image's alpha."""
 	pic = gs.Picture()
 	render.get_renderer().CaptureTexture(tex, pic)
